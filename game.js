9bcae055e6b4fef505103e42cda39aa92909af4e
Daniel Kistemacher da Silva <106277555+DanielKistemacher@users.noreply.github.com>
2023-02-09 22:31:34 -0300

    update

--- game.js
+++ game.js
@@ -1,76 +1,99 @@
 
 function sortearRider() {
-    let conteudoTitulo = document.getElementById("campeao").innerHTML.valueOf;
+    let conteudoTitulo = document.getElementById("campeao");
+    let imagem = document.querySelector("#image");
     const riders = [1, 5, 10, 12, 20, 21, 23, 25, 30, 33, 36, 37, 41, 42, 43, 44, 49, 72, 73, 88, 89, 93];
     let rider = Math.random() * 22;
     let champion = riders[Math.round(rider)];
     
     switch(champion){
         case 1:
-            conteudoTitulo.innerHTML = "Francesco Bagnaia";
+            conteudoTitulo.innerHTML = "Francesco Bagnaia #63";
+            imagem.setAttribute('src', './src/riders/63_Francesco_Bagnaia.png');
             break;
         case 5:
-            conteudoTitulo.innerHTML = "Johann Zarco";
+            conteudoTitulo.innerHTML = "Johann Zarco #05";
+            imagem.setAttribute('src', './src/riders/05_Johann_Zarco.png');
             break;
         case 10:
-            conteudoTitulo.innerHTML = "Luca Marini";
+            conteudoTitulo.innerHTML = "Luca Marini #10";
+            imagem.setAttribute('src', './src/riders/10_Luca_Marini.png');
             break;
         case 12:
-            conteudoTitulo.innerHTML = "Maverick Viñales";
+            conteudoTitulo.innerHTML = "Maverick Viñales #12";
+            imagem.setAttribute('src', './src/riders/12_Maverick_Vinales.png');
             break;
         case 20:
-            conteudoTitulo.innerHTML = "20 Johann Zarco";
+            conteudoTitulo.innerHTML = "Fábio Quartararo #20";
+            imagem.setAttribute('src', './src/riders/20_Fabio_Quartararo.png');
             break;
         case 21:
-            conteudoTitulo.innerHTML = "21 Johann Zarco";
+            conteudoTitulo.innerHTML = "Franco Morbidelli #21";
+            imagem.setAttribute('src', './src/riders/21_Franco_Morbidelli.png');
             break;
         case 23:
-            conteudoTitulo.innerHTML = "23 Johann Zarco";
+            conteudoTitulo.innerHTML = "Enea Bastianini #23";
+            imagem.setAttribute('src', './src/riders/23_Enea_Bastianini.png');
             break;
         case 25:
-            conteudoTitulo.innerHTML = "25 Johann Zarco";
+            conteudoTitulo.innerHTML = "RAul Fernandez #25";
+            imagem.setAttribute('src', './src/riders/25_Raul_Fernandez.png');
             break;
         case 30:
-            conteudoTitulo.innerHTML = "30 Johann Zarco";
+            conteudoTitulo.innerHTML = "Takaaki Nakagami #30";
+            imagem.setAttribute('src', './src/riders/30_Takaaki_Nakagami.png');
             break;
         case 33:
-            conteudoTitulo.innerHTML = "33 Johann Zarco";
+            conteudoTitulo.innerHTML = "Brad Binder #33";
+            imagem.setAttribute('src', './src/riders/33_Brad_Binder.png');
             break;
         case 36:
-            conteudoTitulo.innerHTML = "36 Johann Zarco";
+            conteudoTitulo.innerHTML = "Joan Mir #36";
+            imagem.setAttribute('src', './src/riders/36_Joan_Mir.png');
             break;
         case 37:
-            conteudoTitulo.innerHTML = "37 Johann Zarco";
+            conteudoTitulo.innerHTML = "Augusto Fernandez #37";
+            imagem.setAttribute('src', './src/riders/37_Augusto_Fernandez.png');
             break;
         case 41:
-            conteudoTitulo.innerHTML = "41 Johann Zarco";
+            conteudoTitulo.innerHTML = "Aleix Espargaro #41";
+            imagem.setAttribute('src', './src/riders/41_Aleix_Espargaro.png');
             break;
         case 42:
-            conteudoTitulo.innerHTML = "42 Johann Zarco";
+            conteudoTitulo.innerHTML = "Alex Rins #42";
+            imagem.setAttribute('src', './src/riders/42_Alex_Rins.png');
             break;
         case 43:
-            conteudoTitulo.innerHTML = "43 Johann Zarco";
+            conteudoTitulo.innerHTML = "Jack Miller #43";
+            imagem.setAttribute('src', './src/riders/43_Jack_Miller.png');
             break;
         case 44:
-            conteudoTitulo.innerHTML = "44 Johann Zarco";
+            conteudoTitulo.innerHTML = "Pol Espargaro #44";
+            imagem.setAttribute('src', './src/riders/44_Pol_Espargaro.png');
             break;
         case 49:
-            conteudoTitulo.innerHTML = "49 Johann Zarco";
+            conteudoTitulo.innerHTML = "Fabio Di Giannantonio #49";
+            imagem.setAttribute('src', './src/riders/49_Fabio_DiGiananntonio.png');
             break;
         case 72:
-            conteudoTitulo.innerHTML = "72 Johann Zarco";
+            conteudoTitulo.innerHTML = "Marco Bezzecchi #72";
+            imagem.setAttribute('src', './src/riders/72_Marco_Bezzecchi.png');
             break;
         case 73:
-            conteudoTitulo.innerHTML = "73 Johann Zarco";
+            conteudoTitulo.innerHTML = "Alex Marquez #73";
+            imagem.setAttribute('src', './src/riders/73_Alex_Marquez.png');
             break;
         case 88:
-            conteudoTitulo.innerHTML = "88 Johann Zarco";
+            conteudoTitulo.innerHTML = "Miguel Oliveira #88";
+            imagem.setAttribute('src', './src/riders/88_Miguel_Oliveira.png');
             break;
         case 89:
-            conteudoTitulo.innerHTML = "89 Johann Zarco";
+            conteudoTitulo.innerHTML = "Jorge Martin #89";
+            imagem.setAttribute('src', './src/riders/89_Jorge_Martin.png');
             break;
         case 93:
-            conteudoTitulo.innerHTML = "93 Johann Zarco";
+            conteudoTitulo.innerHTML = "Marc Marquez #93";
+            imagem.setAttribute('src', './src/riders/93_Marc_Marquez.png');
             break;
     }
 }
